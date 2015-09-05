9a6eef02b8543b3292346b7c96382800127b5082
Lucas Lin <lucas.lin177@gmail.com>
2015-09-05 19:18:26 -0400

    removed hardcoded data

--- js/meemaCtrl.js
+++ js/meemaCtrl.js
@@ -3,10 +3,10 @@
 angular.module('app').controller('meemaCtrl',
     ['$scope', 'meemaAuthService', 'meemaWebService',
     function ($scope, meemaAuthService, meemaWebService) {
-        $scope.connected = true;
+        $scope.connected = false;
         $scope.hasDevice = false;
-        $scope.accounts = ['User 1', 'User 2', 'User 3'];
-        $scope.authenticated = true;
+        $scope.accounts = [];
+        $scope.authenticated = false;
         $scope.inputs = null;
         $scope.canSave = false;
         $scope.pageUrl = null;
@@ -25,8 +25,7 @@ angular.module('app').controller('meemaCtrl',
         }; */
 
         $scope.initOptions = function() {
-            $scope.inputs = [{"label":"Username or email","selector":"#id_username.text.long-field","type":"text","input_value":"test","save":true},{"label":"Password","selector":"#id_password.text.long-field","type":"password","input_value":"12345","save":false}];
-            /*getUrl(function(url) {
+            getUrl(function(url) {
                 console.log('got url', url);
                 $scope.pageUrl = url;
                 var params = {
@@ -46,7 +45,7 @@ angular.module('app').controller('meemaCtrl',
                         }
                     }
                 });
-            });*/
+            });
         };
 
         $scope.save = function() {
